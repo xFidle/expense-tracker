5d06a8c262c29d83f76622a50eea10835f209b92
Kamil Marszałek <01187250@pw.edu.pl>
2024-11-30 15:35:43 +0100

    Added ExpenseController

---
+++ ExpenseAPI/src/main/java/com/example/expenseapi/web/ExpenseController.java
@@ -0,0 +1,39 @@
+package com.example.expenseapi.web;
+
+import com.example.expenseapi.pojo.Expense;
+import com.example.expenseapi.service.ExpenseService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/expense")
+public class ExpenseController {
+    ExpenseService expenseService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Expense> getExpense(@PathVariable Long id) {
+        return new ResponseEntity<>(expenseService.getExpense(id), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Expense> saveExpense(@RequestBody Expense expense) {
+        return new ResponseEntity<>(expenseService.saveExpense(expense), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Expense> deleteExpense(@PathVariable Long id) {
+        expenseService.deleteExpense(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Expense>> getExpenses() {
+        return new ResponseEntity<>(expenseService.getAllExpenses(), HttpStatus.OK);
+    }
+}
