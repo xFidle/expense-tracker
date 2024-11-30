e95a00ccd151768432a04d884b9b2fe3899b0cb7
ddsouza <damian.dsouza.stud@pw.edu.pl>
2024-11-30 21:58:55 +0100

    Implemented RecordBlock to display records

--- frontend/composeApp/src/commonMain/kotlin/pw/edu/pl/pap/ui/home/Records.kt
+++ frontend/composeApp/src/commonMain/kotlin/pw/edu/pl/pap/ui/home/Records.kt
@@ -1,2 +1,51 @@
 package pw.edu.pl.pap.ui.home
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import pw.edu.pl.pap.data.Record
+
+@Composable
+fun RecordBlock(record: Record) {
+    Card(
+        shape = RoundedCornerShape(8.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+            .height(50.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = record.user.name,
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Light,
+                color = Color.Gray
+            )
+            Text(
+                text = "${record.price} zł",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Light,
+                color = Color.Gray,
+                textAlign = TextAlign.End
+            )
+        }
+    }
+}
