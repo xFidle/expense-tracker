220f34684456ff9c665a2f0c4a7e0c43561bb402
ddsouza <damian.dsouza.stud@pw.edu.pl>
2024-11-30 21:53:07 +0100

    Added default and min size

--- frontend/composeApp/src/desktopMain/kotlin/pw/edu/pl/pap/main.kt
+++ frontend/composeApp/src/desktopMain/kotlin/pw/edu/pl/pap/main.kt
@@ -1,13 +1,23 @@
 package pw.edu.pl.pap
 
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
+import java.awt.Dimension
 
 fun main() = application {
+    val state = rememberWindowState(
+        width = 600.dp,
+        height = 800.dp,
+    )
+
     Window(
         onCloseRequest = ::exitApplication,
         title = "Expense tracker",
+        state = state,
     ) {
+        window.minimumSize = Dimension(500, 600)
         App()
     }
 }
