14cdeb2482e012fd0cca005b5cff074bf9e6c95c
ddsouza <damian.dsouza.stud@pw.edu.pl>
2024-11-30 18:45:07 +0100

    Created ApiClient class with basic requests

---
+++ frontend/composeApp/src/commonMain/kotlin/pw/edu/pl/pap/apiclient/ApiClient.kt
@@ -0,0 +1,38 @@
+package pw.edu.pl.pap.apiclient
+
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
+import kotlinx.coroutines.flow.flow
+import pw.edu.pl.pap.data.Home
+import pw.edu.pl.pap.data.Record
+
+class ApiClient {
+    private val httpClient = HttpClient {
+        install(ContentNegotiation) {
+            json(Json {
+                prettyPrint = true
+                ignoreUnknownKeys = true
+            })
+        }
+    }
+
+    private val baseUrl = "localhost:8080/"
+
+    suspend fun getHome(userEmail: String): Home {
+        val homeList: List<Home> = httpClient.get(baseUrl + "initial/$userEmail").body()
+
+        return homeList.firstOrNull() ?: throw IllegalStateException("No Home objects found in the response")
+    }
+
+    private suspend fun getRecordsApi(): List<Record> {
+        return httpClient.get(baseUrl + "expense/all").body()
+    }
+
+    fun getRecords() = flow {
+        emit(getRecordsApi())
+    }
+}
