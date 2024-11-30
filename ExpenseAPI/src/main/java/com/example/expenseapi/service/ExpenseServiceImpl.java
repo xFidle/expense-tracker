dfbf7c433e70ff71f56e0e5def0ee6dfecc0f252
Kamil Marszałek <01187250@pw.edu.pl>
2024-11-30 23:59:54 +0100

    Fixed save expense bug

--- ExpenseAPI/src/main/java/com/example/expenseapi/service/ExpenseServiceImpl.java
+++ ExpenseAPI/src/main/java/com/example/expenseapi/service/ExpenseServiceImpl.java
@@ -1,7 +1,9 @@
 package com.example.expenseapi.service;
 
+import com.example.expenseapi.pojo.Category;
 import com.example.expenseapi.pojo.Expense;
 import com.example.expenseapi.pojo.ExpenseNotFound;
+import com.example.expenseapi.repository.CategoryRepository;
 import com.example.expenseapi.repository.ExpenseRepository;
 import org.springframework.stereotype.Service;
 import java.util.List;
@@ -11,10 +13,12 @@ import java.util.stream.StreamSupport;
 @Service
 public class ExpenseServiceImpl extends GenericServiceImpl<Expense, Long> implements ExpenseService {
     private final ExpenseRepository expenseRepository;
+    private final CategoryRepository categoryRepository;
 
-    public ExpenseServiceImpl(ExpenseRepository repository) {
+    public ExpenseServiceImpl(ExpenseRepository repository, CategoryRepository categoryRepository) {
         super(repository);
         this.expenseRepository = repository;
+        this.categoryRepository = categoryRepository;
     }
 
     public List<Expense> getExpensesByEmail(String mail) {
@@ -22,5 +26,13 @@ public class ExpenseServiceImpl extends GenericServiceImpl<Expense, Long> implem
         return StreamSupport.stream(expanses.spliterator(), false)
                 .filter(expanse -> expanse.getUser().getEmail().equals(mail))
                 .collect(Collectors.toList());
+    }
+
+    @Override
+    public Expense save(Expense entity) {
+        if (entity.getCategory() == null) {
+            entity.setCategory(categoryRepository.findById(1L).get());
+        }
+        return super.save(entity);
     }
 }
