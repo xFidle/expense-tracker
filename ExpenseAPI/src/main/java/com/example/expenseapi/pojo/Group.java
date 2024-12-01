b3978aaaebbc58889f05fefe5265f9587b5ebaa6
Kamil Marszałek <01187250@pw.edu.pl>
2024-12-01 13:28:52 +0100

    Created Group entity

---
+++ ExpenseAPI/src/main/java/com/example/expenseapi/pojo/Group.java
@@ -0,0 +1,27 @@
+package com.example.expenseapi.pojo;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "group")
+public class Group {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name", nullable = false, columnDefinition = "VARCHAR2(255)")
+    private String name = "family";
+
+    public Group(String name) {
+        this.name = name;
+    }
+}
