d661e8856359b7d0e0adb7366ccce4adaa5c05da
msuski <01187288@pw.edu.pl>
2024-12-01 11:24:06 +0100

    improved addButton parameters

--- frontend/composeApp/src/commonMain/kotlin/pw/edu/pl/pap/ui/home/PlusButton.kt
+++ frontend/composeApp/src/commonMain/kotlin/pw/edu/pl/pap/ui/home/PlusButton.kt
@@ -1,25 +1,33 @@
 package pw.edu.pl.pap.ui.home
 
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import 	androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonColors
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun PlusButton(): Unit {
     Button(
         shape = RoundedCornerShape(100),
         modifier = Modifier
-            .width(50.dp)
-            .height(50.dp),
+            .width(60.dp)
+            .height(60.dp),
+        colors = ButtonColors(Color.DarkGray, Color.Cyan, Color.DarkGray, Color.Cyan),
+        contentPadding = PaddingValues(0.dp),
         onClick = {}
     ) {
-        Text("+")
+        Text(
+            text = "+",
+            fontSize = 50.sp,
+            fontWeight = FontWeight.Medium,
+            modifier = Modifier.offset(x = 0.dp, y = (-10).dp)
+        )
     }
 }
