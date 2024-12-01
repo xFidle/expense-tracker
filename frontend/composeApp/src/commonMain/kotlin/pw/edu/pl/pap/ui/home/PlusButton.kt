c2ac6ef788b51d3c0201d2e2394120000754bef3
msuski <01187288@pw.edu.pl>
2024-12-01 11:37:15 +0100

    placed addButton in correct position

--- frontend/composeApp/src/commonMain/kotlin/pw/edu/pl/pap/ui/home/PlusButton.kt
+++ frontend/composeApp/src/commonMain/kotlin/pw/edu/pl/pap/ui/home/PlusButton.kt
@@ -4,30 +4,39 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import 	androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ButtonColors
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.*
 
 @Composable
 fun PlusButton(): Unit {
-    Button(
-        shape = RoundedCornerShape(100),
-        modifier = Modifier
-            .width(60.dp)
-            .height(60.dp),
-        colors = ButtonColors(Color.DarkGray, Color.Cyan, Color.DarkGray, Color.Cyan),
-        contentPadding = PaddingValues(0.dp),
-        onClick = {}
+    Box(
+        modifier = Modifier.fillMaxSize()
     ) {
-        Text(
-            text = "+",
-            fontSize = 50.sp,
-            fontWeight = FontWeight.Medium,
-            modifier = Modifier.offset(x = 0.dp, y = (-10).dp)
-        )
+        Button(
+            shape = RoundedCornerShape(100),
+            modifier = Modifier
+                .width(60.dp)
+                .height(60.dp)
+                .align(Alignment.BottomEnd)
+                .offset(x= (-20).dp, y= (-20).dp),
+//                .padding(16.dp),
+            colors = ButtonColors(Color.DarkGray, Color.Cyan, Color.DarkGray, Color.Cyan),
+            contentPadding = PaddingValues(0.dp),
+            onClick = {}
+        ) {
+            Text(
+                text = "+",
+                fontSize = 50.sp,
+                fontWeight = FontWeight.Medium,
+                modifier = Modifier.offset(x = 0.dp, y = (-10).dp)
+            )
+        }
     }
+
 }
