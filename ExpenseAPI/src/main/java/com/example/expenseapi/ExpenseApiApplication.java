38e65a3af17b4997a469feaf71b4149af916943c
Kamil Marszałek <01187250@pw.edu.pl>
2024-12-01 14:25:21 +0100

    Adjusted creating initial users

--- ExpenseAPI/src/main/java/com/example/expenseapi/ExpenseApiApplication.java
+++ ExpenseAPI/src/main/java/com/example/expenseapi/ExpenseApiApplication.java
@@ -1,9 +1,11 @@
 package com.example.expenseapi;
 
 import com.example.expenseapi.pojo.Expense;
+import com.example.expenseapi.pojo.UserGroup;
 import com.example.expenseapi.pojo.User;
 import com.example.expenseapi.pojo.Category;
 import com.example.expenseapi.repository.ExpenseRepository;
+import com.example.expenseapi.repository.UserGroupRepository;
 import com.example.expenseapi.repository.UserRepository;
 import com.example.expenseapi.repository.CategoryRepository;
 import org.springframework.boot.CommandLineRunner;
@@ -17,11 +19,13 @@ public class ExpenseApiApplication implements CommandLineRunner {
     final ExpenseRepository expenseRepository;
     final UserRepository userRepository;
     private final CategoryRepository categoryRepository;
+    private final UserGroupRepository groupRepository;
 
-    public ExpenseApiApplication(ExpenseRepository expenseRepository, UserRepository userRepository, CategoryRepository categoryRepository) {
+    public ExpenseApiApplication(ExpenseRepository expenseRepository, UserRepository userRepository, CategoryRepository categoryRepository, UserGroupRepository groupRepository) {
         this.expenseRepository = expenseRepository;
         this.userRepository = userRepository;
         this.categoryRepository = categoryRepository;
+        this.groupRepository = groupRepository;
     }
 
     public static void main(String[] args) {
@@ -30,10 +34,15 @@ public class ExpenseApiApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
+        UserGroup[] groups = new UserGroup[]{
+                new UserGroup("family"),
+                new UserGroup("workers"),
+        };
+        groupRepository.saveAll(Arrays.asList(groups));
         User[] users = new User[]{
-            new User("Herkules1", "Herkules1", "herkules1@gmail.com"),
-            new User("Herkules2", "Herkules2", "herkules2@gmail.com"),
-            new User("Herkules3", "Herkules3", "herkules3@gmail.com")
+            new User("Herkules1", "Herkules1", "herkules1@gmail.com", groups[0]),
+            new User("Herkules2", "Herkules2", "herkules2@gmail.com", groups[1]),
+            new User("Herkules3", "Herkules3", "herkules3@gmail.com" , groups[0]),
         };
         userRepository.saveAll(Arrays.asList(users));
 
