bebb88279206ebcdacd6ec77bbf1b6ea64734660
Kamil Marszałek <01187250@pw.edu.pl>
2024-12-18 22:41:26 +0100

    Implemented currency rates fetcher

---
+++ ExpenseAPI/src/main/java/com/example/expenseapi/utils/CurrencyRatesFetcher.java
@@ -0,0 +1,31 @@
+package com.example.expenseapi.utils;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.List;
+
+public class CurrencyRatesFetcher {
+
+    public static double getCurrencyRates(String currencyCode) {
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            String url = String.format("https://api.nbp.pl/api/exchangerates/rates/A/%s?format=json", currencyCode);
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    .GET()
+                    .build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode root = mapper.readTree(response.body());
+            double rate = root.get("rates").get(0).get("mid").asDouble();
+            return rate;
+        } catch (Exception e) {
+            return -1;
+        }
+    }
+}
