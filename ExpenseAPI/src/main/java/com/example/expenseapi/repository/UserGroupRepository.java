8debdde23a4f712be010d12cff369b6f0581b579
Kamil Marszałek <01187250@pw.edu.pl>
2024-12-18 21:27:33 +0100

    Refactoring after changing user group to group

--- ExpenseAPI/src/main/java/com/example/expenseapi/repository/UserGroupRepository.java
+++ ExpenseAPI/src/main/java/com/example/expenseapi/repository/UserGroupRepository.java
@@ -1,6 +1,6 @@
 package com.example.expenseapi.repository;
 
-import com.example.expenseapi.pojo.UserGroup;
+import com.example.expenseapi.pojo.Group;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserGroupRepository extends CrudRepository<UserGroup, Long> {}
+public interface UserGroupRepository extends CrudRepository<Group, Long> {}
