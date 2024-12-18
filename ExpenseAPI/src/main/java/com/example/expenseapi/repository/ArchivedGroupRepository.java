53ef33f913428872c24b24392ca634922eaa0946
Kamil Marszałek <01187250@pw.edu.pl>
2024-12-18 21:34:17 +0100

    Added ArchivedGroupRepository

---
+++ ExpenseAPI/src/main/java/com/example/expenseapi/repository/ArchivedGroupRepository.java
@@ -0,0 +1,6 @@
+package com.example.expenseapi.repository;
+
+import com.example.expenseapi.pojo.ArchivedGroup;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ArchivedGroupRepository extends CrudRepository<ArchivedGroup, Long> {}
