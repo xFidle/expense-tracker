ec934896675b22a61197ddb2c26042b03060a22a
Kamil Marszałek <01187250@pw.edu.pl>
2024-12-21 18:43:30 +0100

    Implemented findByEmail method

--- ExpenseAPI/src/main/java/com/example/expenseapi/service/UserServiceImpl.java
+++ ExpenseAPI/src/main/java/com/example/expenseapi/service/UserServiceImpl.java
@@ -4,10 +4,20 @@ import com.example.expenseapi.pojo.User;
 import com.example.expenseapi.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 
 @Service
 public class UserServiceImpl extends GenericServiceImpl<User, Long> implements UserService {
-    public UserServiceImpl(UserRepository repository) {
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository repository, UserRepository userRepository) {
         super(repository);
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        return userRepository.findByEmail(email);
     }
 }
