4ca967c53e29cea1625dd4448b299126075c6bc4
Kamil Marszałek <01187250@pw.edu.pl>
2024-12-21 11:31:42 +0100

    Got rid of warnings

--- ExpenseAPI/src/main/java/com/example/expenseapi/utils/JwtAuthenticationFilter.java
+++ ExpenseAPI/src/main/java/com/example/expenseapi/utils/JwtAuthenticationFilter.java
@@ -1,11 +1,10 @@
 package com.example.expenseapi.utils;
 
-import com.example.expenseapi.service.UserService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -26,7 +25,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, @NonNull HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         final String authorizationHeader = request.getHeader("Authorization");
 
         String email = null;
