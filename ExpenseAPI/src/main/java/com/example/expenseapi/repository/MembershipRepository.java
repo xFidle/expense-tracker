e86c92cb602e8eb3cf68cb16c35249705067aaf0
Kamil Marszałek <01187250@pw.edu.pl>
2024-12-21 18:42:11 +0100

    Added MembershipRepository

--- ExpenseAPI/src/main/java/com/example/expenseapi/repository/MembershipRepository.java
+++ ExpenseAPI/src/main/java/com/example/expenseapi/repository/MembershipRepository.java
@@ -1,8 +1,15 @@
 package com.example.expenseapi.repository;
 
+import com.example.expenseapi.pojo.BaseGroup;
 import com.example.expenseapi.pojo.Membership;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 
 public interface MembershipRepository extends JpaRepository<Membership, Long> {
+    @Query("SELECT m.group FROM Membership m WHERE m.user.id = :userId")
+    List<BaseGroup> findBaseGroupsByUser_Id(@Param("userId") Long userId);
 }
