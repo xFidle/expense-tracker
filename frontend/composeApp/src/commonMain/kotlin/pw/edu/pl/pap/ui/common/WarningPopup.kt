a5b2518adc6164354950fb679f46947324547915
msuski <01187288@pw.edu.pl>
2024-12-22 14:58:42 +0100

    Implemented WarningPopup

---
+++ frontend/composeApp/src/commonMain/kotlin/pw/edu/pl/pap/ui/common/WarningPopup.kt
@@ -0,0 +1,25 @@
+package pw.edu.pl.pap.ui.common
+
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+
+@Composable
+fun WarningPopup(
+    mainText: String = "Warning",
+    subText: String,
+    onDismiss: () -> Unit
+) {
+    AlertDialog(
+        onDismissRequest = {onDismiss()}, // Prevent dismissal by user actions
+        title = {
+            Text(mainText)
+        },
+        text = {
+            Text(subText)
+        },
+        confirmButton = {}, // No confirm button for a loading dialog
+        dismissButton = {}  // No dismiss button for a loading dialog
+    )
+}
