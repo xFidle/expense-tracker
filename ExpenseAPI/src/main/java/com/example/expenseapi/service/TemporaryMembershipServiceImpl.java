246c3f41797c91497fb8cb13cb5532b0d22273c9
Kamil Marszałek <01187250@pw.edu.pl>
2024-12-28 20:44:09 +0100

    Added @Service annotation

--- ExpenseAPI/src/main/java/com/example/expenseapi/service/TemporaryMembershipServiceImpl.java
+++ ExpenseAPI/src/main/java/com/example/expenseapi/service/TemporaryMembershipServiceImpl.java
@@ -2,7 +2,9 @@ package com.example.expenseapi.service;
 
 import com.example.expenseapi.pojo.TemporaryMembership;
 import com.example.expenseapi.repository.TemporaryMembershipRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public class TemporaryMembershipServiceImpl extends GenericServiceImpl<TemporaryMembership, Long> implements TemporaryMembershipService {
     TemporaryMembershipRepository temporaryMembershipRepository;
     public TemporaryMembershipServiceImpl(TemporaryMembershipRepository repository) {
