1166ff0fb9d875e688e84d8418c5f8654c298b17
Kamil Marszałek <01187250@pw.edu.pl>
2024-12-28 18:12:27 +0100

    Made a query to retrieve admins from database

--- ExpenseAPI/src/main/java/com/example/expenseapi/repository/MembershipRepository.java
+++ ExpenseAPI/src/main/java/com/example/expenseapi/repository/MembershipRepository.java
@@ -2,6 +2,7 @@ package com.example.expenseapi.repository;
 
 import com.example.expenseapi.pojo.BaseGroup;
 import com.example.expenseapi.pojo.Membership;
+import com.example.expenseapi.pojo.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,5 +13,9 @@ import java.util.List;
 public interface MembershipRepository extends JpaRepository<Membership, Long> {
     @Query("SELECT m.group FROM Membership m WHERE m.user.id = :userId")
     List<BaseGroup> findBaseGroupsByUser_Id(@Param("userId") Long userId);
+
+    @Query("select m.user from Membership m where m.role.id = 1L and m.group.name = :group")
+    List<User> findAdmins(String group);
+
     List<Membership> findByUserId(Long userId);
 }
