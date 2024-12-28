32b900ba0f049b658445762b49c19e3cbe926365
Kamil Marszałek <01187250@pw.edu.pl>
2024-12-28 22:43:50 +0100

    Changed add endpoint to invite endpoint

--- ExpenseAPI/src/main/java/com/example/expenseapi/web/MembershipController.java
+++ ExpenseAPI/src/main/java/com/example/expenseapi/web/MembershipController.java
@@ -1,8 +1,11 @@
 package com.example.expenseapi.web;
 
+import com.example.expenseapi.pojo.BaseMembership;
 import com.example.expenseapi.pojo.Membership;
+import com.example.expenseapi.pojo.TemporaryMembership;
 import com.example.expenseapi.pojo.User;
 import com.example.expenseapi.service.MembershipService;
+import com.example.expenseapi.service.TemporaryMembershipService;
 import com.example.expenseapi.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,19 +17,21 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/membership")
 public class MembershipController extends GenericController<Membership, Long> {
     private final MembershipService membershipService ;
+    private final TemporaryMembershipService temporaryMembershipService;
     private final UserService userService ;
-    public MembershipController(MembershipService service, MembershipService membershipService, UserService userService) {
+    public MembershipController(MembershipService service, MembershipService membershipService, UserService userService, TemporaryMembershipService temporaryMembershipService) {
         super(service);
         this.membershipService = membershipService;
         this.userService = userService;
+        this.temporaryMembershipService = temporaryMembershipService;
     }
 
-    @PostMapping("/add")
-    public ResponseEntity<HttpStatus> save(@RequestBody Membership entity, @AuthenticationPrincipal org.springframework.security.core.userdetails.User user) {
+    @PostMapping("/invite")
+    public ResponseEntity<HttpStatus> save(@RequestBody BaseMembership entity, @AuthenticationPrincipal org.springframework.security.core.userdetails.User user) {
         String email = user.getUsername();
         User mUser = userService.findByEmail(email).get();
         if (((MembershipService) service).getRole(mUser, entity).equals("admin")) {
-            membershipService.save(entity);
+            temporaryMembershipService.save(new TemporaryMembership(entity));
             return new ResponseEntity<>(HttpStatus.OK);
         }
         return new ResponseEntity<>(HttpStatus.FORBIDDEN);
