70fc93b8e39119e6f06faeb2ef0758b8d3e0ea5d
msuski <01187288@pw.edu.pl>
2024-12-29 23:29:10 +0100

    Implemented setup fields for SettingsScreenComponent

--- frontend/composeApp/src/commonMain/kotlin/pw/edu/pl/pap/screenComponents/mainScreens/SettingsScreenComponent.kt
+++ frontend/composeApp/src/commonMain/kotlin/pw/edu/pl/pap/screenComponents/mainScreens/SettingsScreenComponent.kt
@@ -1,8 +1,77 @@
 package pw.edu.pl.pap.screenComponents.mainScreens
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Modifier
+import kotlinx.coroutines.launch
+import pw.edu.pl.pap.data.uiSetup.inputFields.ButtonData
+import pw.edu.pl.pap.data.uiSetup.inputFields.InputFieldData
+import pw.edu.pl.pap.data.uiSetup.inputFields.TextFieldData
+
 class SettingsScreenComponent(
     baseComponent: BaseScreenComponent
-) : BaseScreenComponent by baseComponent
+) : BaseScreenComponent by baseComponent {
+
+    private val _inputFieldsData = mutableStateListOf<InputFieldData>()
+    val inputFieldsData: List<InputFieldData> get() = _inputFieldsData
+
+//    private var serverAddress: MutableState<String> = mutableStateOf(apiService.baseUrl)
+    // TODO
+    private var serverAddress: MutableState<String> = mutableStateOf("")
+
+
+    fun setupInputFields() {
+        _inputFieldsData.clear()
+        _inputFieldsData.addAll(
+            listOf(
+                InputFieldData(
+                    title = "Server: ",
+                    textFieldData = TextFieldData(
+                        parameter = serverAddress,
+                        onChange = {
+                            coroutineScope.launch { serverAddress.value = it }
+                        }
+                    )
+                ),
+                InputFieldData(
+                    title = "",
+                    isButton = true,
+                    buttonData = ButtonData(
+                        title = "Edit personal user data",
+                        onClick = {},
+                    )
+                ),
+                InputFieldData(
+                    title = "",
+                    isButton = true,
+                    buttonData = ButtonData(
+                        title = "Change password",
+                        onClick = {},
+                    )
+                ),
+                InputFieldData(
+                    title = "",
+                    isButton = true,
+                    buttonData = ButtonData(
+                        title = "Edit preferences",
+                        onClick = {},
+                    )
+                ),
+                InputFieldData(
+                    title = "",
+                    isButton = true,
+                    buttonData = ButtonData(
+                        title = "LOG OUT",
+                        onClick = {},
+                    )
+                ),
+            )
+        )
+    }
+}
+
+
 
 //TODO set server address
 //TODO log out
