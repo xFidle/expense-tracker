6813d040bbbb6b22044d8336a86fae6b80af35b8
Kamil Marszałek <01187250@pw.edu.pl>
2024-12-29 13:57:11 +0100

    Improved FilterController

--- ExpenseAPI/src/main/java/com/example/expenseapi/web/FilterController.java
+++ ExpenseAPI/src/main/java/com/example/expenseapi/web/FilterController.java
@@ -10,10 +10,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
