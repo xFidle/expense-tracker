7887f5c0dccae7a01cf1e3d3f77c455e0326d568
Kamil Marszałek <01187250@pw.edu.pl>
2024-12-31 14:49:31 +0100

    Added method needed for mapping

--- ExpenseAPI/src/main/java/com/example/expenseapi/repository/MethodOfPaymentRepository.java
+++ ExpenseAPI/src/main/java/com/example/expenseapi/repository/MethodOfPaymentRepository.java
@@ -4,4 +4,5 @@ import com.example.expenseapi.pojo.MethodOfPayment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MethodOfPaymentRepository extends JpaRepository<MethodOfPayment, Long> {
+    MethodOfPayment findByName(String methodOfPayment);
 }
