d7d515ffbd4faf54ed1f6a070d67d36ba402b6e5
Kamil Marszałek <01187250@pw.edu.pl>
2025-01-01 13:41:31 +0100

    Created UserDTO not to send password

---
+++ ExpenseAPI/src/main/java/com/example/expenseapi/dto/UserDTO.java
@@ -0,0 +1,17 @@
+package com.example.expenseapi.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+    private String name;
+    private String surname;
+    private String email;
+}
