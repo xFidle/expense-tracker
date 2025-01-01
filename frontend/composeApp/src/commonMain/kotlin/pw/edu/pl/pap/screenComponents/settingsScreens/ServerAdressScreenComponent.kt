ece47cba4592c5df25c84f66de4f6ebbf16cc744
msuski <01187288@pw.edu.pl>
2025-01-01 14:37:17 +0100

    created ServerAdressScreenComponent

---
+++ frontend/composeApp/src/commonMain/kotlin/pw/edu/pl/pap/screenComponents/settingsScreens/ServerAdressScreenComponent.kt
@@ -0,0 +1,8 @@
+package pw.edu.pl.pap.screenComponents.settingsScreens
+
+
+class ServerAdressScreenComponent (
+    baseSettingsScreenComponent: BaseSettingsScreenComponent
+) : BaseSettingsScreenComponentImpl(baseSettingsScreenComponent) {
+
+}
