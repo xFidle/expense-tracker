0a8b389b1c50a0b1100d1a24dd7bafffe384278a
Kamil Marszałek <kamilmar553@gmail.com>
2025-01-01 18:42:06 +0100

    Implemented CurrencyController

--- ExpenseAPI/src/main/java/com/example/expenseapi/web/CurrencyController.java
+++ ExpenseAPI/src/main/java/com/example/expenseapi/web/CurrencyController.java
@@ -1,3 +1,15 @@
-//public class CurrencyController extends GenericController<Currency, Long> {
-//
-//}
+package com.example.expenseapi.web;
+
+import com.example.expenseapi.service.CurrencyService;
+import com.example.expenseapi.pojo.Currency;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/currency")
+public class CurrencyController extends GenericController<Currency, Long> {
+    public CurrencyController(CurrencyService service) {
+        super(service);
+    }
+}
