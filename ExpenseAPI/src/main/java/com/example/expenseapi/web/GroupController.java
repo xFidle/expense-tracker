3c56da3dcda69be058f133b4e3ed997a28a95055
Kamil Marszałek <01187250@pw.edu.pl>
2025-01-02 19:08:44 +0100

    Removed unused imports

--- ExpenseAPI/src/main/java/com/example/expenseapi/web/GroupController.java
+++ ExpenseAPI/src/main/java/com/example/expenseapi/web/GroupController.java
@@ -1,14 +1,10 @@
 package com.example.expenseapi.web;
 
-import com.example.expenseapi.pojo.BaseGroup;
 import com.example.expenseapi.pojo.Group;
 import com.example.expenseapi.service.MembershipService;
 import com.example.expenseapi.service.UserGroupService;
-import com.example.expenseapi.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
