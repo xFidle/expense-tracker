2ca7a8f178f89cb35ff7ac2b943e3cc42e3cf8dc
Kamil Marszałek <01187250@pw.edu.pl>
2025-01-03 09:48:17 +0100

    Added tag to CurrencyController

--- ExpenseAPI/src/main/java/com/example/expenseapi/web/CurrencyController.java
+++ ExpenseAPI/src/main/java/com/example/expenseapi/web/CurrencyController.java
@@ -2,12 +2,12 @@ package com.example.expenseapi.web;
 
 import com.example.expenseapi.service.CurrencyService;
 import com.example.expenseapi.pojo.Currency;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/currency")
+@Tag(name="Currency Controller", description = "Controller to handle currency operations")
 public class CurrencyController extends GenericController<Currency, Long> {
     public CurrencyController(CurrencyService service) {
         super(service);
