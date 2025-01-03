ed53a211f56e5d31fc0dcf749d7d6ba2ab9f6a38
Kamil Marszałek <01187250@pw.edu.pl>
2025-01-03 18:56:14 +0100

    Declared method to search users

--- ExpenseAPI/src/main/java/com/example/expenseapi/service/UserService.java
+++ ExpenseAPI/src/main/java/com/example/expenseapi/service/UserService.java
@@ -1,9 +1,13 @@
 package com.example.expenseapi.service;
 
+import com.example.expenseapi.dto.UserDTO;
+import com.example.expenseapi.dto.UserFilter;
 import com.example.expenseapi.pojo.User;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserService extends GenericService<User, Long> {
     Optional<User> findByEmail(String email);
+    List<UserDTO> searchUsersDTO(UserFilter filter);
 }
