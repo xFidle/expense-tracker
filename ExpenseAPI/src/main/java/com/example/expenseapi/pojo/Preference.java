37962b8277c485dce6258ee1f9b99751b979b137
Michał Szwejk <michal.szwejk.stud@pw.edu.pl>
2025-01-03 22:11:07 +0100

    Preference pojo implemented

---
+++ ExpenseAPI/src/main/java/com/example/expenseapi/pojo/Preference.java
@@ -0,0 +1,48 @@
+package com.example.expenseapi.pojo;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "preferences")
+public class Preference {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @NonNull
+    @OneToOne(optional = false)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    @NonNull
+    @JoinColumn(name = "currency_id", referencedColumnName = "id")
+    @ManyToOne(optional = false)
+    private Currency currency;
+
+    @NonNull
+    @JoinColumn(name = "method_id", referencedColumnName = "id")
+    @ManyToOne(optional = false)
+    private MethodOfPayment method;
+
+    @NonNull
+    @Column(name = "language", nullable = false)
+    private String language = "pl";
+
+    public Preference(@NonNull User user, @NonNull Currency currency,@NonNull MethodOfPayment method) {
+        this.user = user;
+        this.currency = currency;
+        this.method = method;
+    }
+
+    public Preference(@NonNull User user, @NonNull Currency currency,@NonNull MethodOfPayment method, @NonNull String language) {
+        this(user, currency, method);
+        this.language = language;
+    }
+}
