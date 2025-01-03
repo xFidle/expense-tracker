4ca52e7e31be27b6dec4fc8f4263d923956a9aba
Kamil Marszałek <01187250@pw.edu.pl>
2025-01-03 09:21:48 +0100

    Refactored MembershipService to return list of user dto

--- ExpenseAPI/src/main/java/com/example/expenseapi/service/MembershipService.java
+++ ExpenseAPI/src/main/java/com/example/expenseapi/service/MembershipService.java
@@ -1,5 +1,6 @@
 package com.example.expenseapi.service;
 
+import com.example.expenseapi.dto.UserDTO;
 import com.example.expenseapi.pojo.BaseGroup;
 import com.example.expenseapi.pojo.BaseMembership;
 import com.example.expenseapi.pojo.Membership;
@@ -10,7 +11,7 @@ import java.util.List;
 public interface MembershipService extends GenericService<Membership, Long> {
     List<BaseGroup> getBaseGroupsByUserId(Long userId);
     List<Membership> getMembershipsByUserId(Long userId);
-    List<User> findAdmins(String group);
-    List<User> findUsers(String group);
+    List<UserDTO> findAdmins(String group);
+    List<UserDTO> findUsers(String group);
     String getRole(User user, BaseMembership entity);
 }
