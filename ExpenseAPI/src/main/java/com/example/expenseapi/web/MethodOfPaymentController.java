9c4d15838290ca22bffd44a6ee9bebc1edc82db6
Kamil Marszałek <01187250@pw.edu.pl>
2025-01-03 10:15:51 +0100

    Added docs for MethodOfPaymentController controller

--- ExpenseAPI/src/main/java/com/example/expenseapi/web/MethodOfPaymentController.java
+++ ExpenseAPI/src/main/java/com/example/expenseapi/web/MethodOfPaymentController.java
@@ -2,11 +2,13 @@ package com.example.expenseapi.web;
 
 import com.example.expenseapi.pojo.MethodOfPayment;
 import com.example.expenseapi.service.MethodOfPaymentService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/method")
+@Tag(name = "Payment Method Controller", description = "Controller to manage payment methods")
 public class MethodOfPaymentController extends GenericController<MethodOfPayment, Long> {
     public MethodOfPaymentController(MethodOfPaymentService service) {
         super(service);
