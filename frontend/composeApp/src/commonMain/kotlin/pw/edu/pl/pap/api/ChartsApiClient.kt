780051f8bb982ddfbb193211f6548ce522178312
ddsouza <damian.dsouza.stud@pw.edu.pl>
2025-01-03 14:58:43 +0100

    Updated charts api and endpoints

--- frontend/composeApp/src/commonMain/kotlin/pw/edu/pl/pap/api/ChartsApiClient.kt
+++ frontend/composeApp/src/commonMain/kotlin/pw/edu/pl/pap/api/ChartsApiClient.kt
@@ -1,27 +1,15 @@
 package pw.edu.pl.pap.api
 
 import io.ktor.client.call.*
-import kotlinx.datetime.LocalDate
 import pw.edu.pl.pap.api.endpoints.ChartEndpoint
+import pw.edu.pl.pap.data.databaseAssociatedData.ChartFilterParams
 import pw.edu.pl.pap.util.charts.ChartData
 
 class ChartsApiClient(baseApiClient: BaseApiClient) :
     ApiClient by baseApiClient {
 
-    suspend fun getGroupYearData(year: Number): ChartData {
-        val map: Map<String, Float> = get(ChartEndpoint.GroupYearData(year)).body()
+    suspend fun getData(group: String, keyPattern: String, filterParams: ChartFilterParams): ChartData {
+        val map: Map<String, Float> = get(ChartEndpoint.FilteredData(group, keyPattern, filterParams)).body()
         return map.toMap(ChartData())
-    }
-
-    suspend fun getGroupCategoriesPeriod(begin: LocalDate, end: LocalDate): ChartData {
-        return get(ChartEndpoint.GroupCategoriesPeriod(begin, end)).body()
-    }
-
-    suspend fun getUserYearData(year: Number): ChartData {
-        return get(ChartEndpoint.UserYearData(year)).body()
-    }
-
-    suspend fun getUserCategoriesPeriod(begin: LocalDate, end: LocalDate): ChartData {
-        return get(ChartEndpoint.UserCategoriesPeriod(begin, end)).body()
     }
 }
