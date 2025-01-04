ced851937a54bd1cc27638043eb2a7c18a8410cc
Kamil Marszałek <01187250@pw.edu.pl>
2025-01-04 22:00:33 +0100

    Created DTO to update password

---
+++ ExpenseAPI/src/main/java/com/example/expenseapi/dto/ChangePasswordDTO.java
@@ -0,0 +1,15 @@
+package com.example.expenseapi.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChangePasswordDTO {
+    private String oldPassword;
+    private String newPassword;
+}
