e03debfaa5ebfdf3a1d42a16a191acc62eebd040
ddsouza <damian.dsouza.stud@pw.edu.pl>
2025-01-06 00:10:48 +0100

    finished ChartsFilterScreen ui

--- frontend/composeApp/src/commonMain/kotlin/pw/edu/pl/pap/ui/chartsScreen/filterScreen/ChartsFilterScreen.kt
+++ frontend/composeApp/src/commonMain/kotlin/pw/edu/pl/pap/ui/chartsScreen/filterScreen/ChartsFilterScreen.kt
@@ -3,10 +3,15 @@ package pw.edu.pl.pap.ui.chartsScreen.filterScreen
 import androidx.compose.runtime.*
 import pw.edu.pl.pap.screenComponents.chartsScreens.ChartsFilterScreenComponent
 import pw.edu.pl.pap.ui.common.Header
+import pw.edu.pl.pap.ui.common.InputFields
 
 @Composable
 fun ChartsFilterScreen(component: ChartsFilterScreenComponent) {
     Header("Filter data")
 
+    InputFields(component.inputFieldsData)
 
+    ChartsFilterScreenButtonRow(
+        onBack = component.onDismiss, onConfirm = component::confirm
+    )
 }
