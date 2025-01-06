30592b62637a4c1b4a9d671f5b3e3560581e7c78
Kamil Marszałek <01187250@pw.edu.pl>
2025-01-06 23:44:14 +0100

    Created ArchivedMembership

---
+++ ExpenseAPI/src/main/java/com/example/expenseapi/pojo/ArchivedMembership.java
@@ -0,0 +1,23 @@
+package com.example.expenseapi.pojo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Entity
+@Table(name="archived_memberships")
+public class ArchivedMembership extends BaseMembership<ArchivedGroup>{
+    public ArchivedMembership(User user, ArchivedGroup group, Role role) {
+        super(user, group, role);
+    }
+
+    public ArchivedMembership(BaseMembership<ArchivedGroup> entity) {
+        super(entity.user, entity.group, entity.role);
+    }
+}
