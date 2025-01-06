86be39fd22e2298bfff8db65d8f24df662b60d13
Michał Szwejk <michal.szwejk.stud@pw.edu.pl>
2025-01-06 14:37:25 +0100

    moved GlobalExceptionHandler to utils

--- ExpenseAPI/src/main/java/com/example/expenseapi/utils/GlobalExceptionHandler.java
+++ ExpenseAPI/src/main/java/com/example/expenseapi/utils/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
-package com.example.expenseapi.exception;
+package com.example.expenseapi.utils;
 
+import com.example.expenseapi.exception.BadRequestException;
+import com.example.expenseapi.exception.ForbiddenException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
