8fc836f7d1d7fdf32f4d0640e10fbbbe5faf0e75
Kamil Marszałek <01187250@pw.edu.pl>
2025-01-06 23:46:47 +0100

    Modified TemporaryMembership

--- ExpenseAPI/src/main/java/com/example/expenseapi/pojo/TemporaryMembership.java
+++ ExpenseAPI/src/main/java/com/example/expenseapi/pojo/TemporaryMembership.java
@@ -11,12 +11,12 @@ import lombok.Setter;
 @NoArgsConstructor
 @Entity
 @Table(name = "temporary_memberships")
-public class TemporaryMembership extends BaseMembership {
-    public TemporaryMembership(User user, BaseGroup group, Role role) {
+public class TemporaryMembership extends BaseMembership<Group> {
+    public TemporaryMembership(User user, Group group, Role role) {
         super(user, group, role);
     }
 
-    public TemporaryMembership(BaseMembership entity) {
+    public TemporaryMembership(BaseMembership<Group> entity) {
         super(entity.user, entity.group, entity.role);
     }
 }
