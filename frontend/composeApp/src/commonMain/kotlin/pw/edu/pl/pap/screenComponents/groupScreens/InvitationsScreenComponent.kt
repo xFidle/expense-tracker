74bcd2a6af1c3a8f5aa2133b2f51f1232c0b14b0
msuski <01187288@pw.edu.pl>
2025-01-07 16:13:59 +0100

    Created InvitationsScreenComponent

---
+++ frontend/composeApp/src/commonMain/kotlin/pw/edu/pl/pap/screenComponents/groupScreens/InvitationsScreenComponent.kt
@@ -0,0 +1,48 @@
+package pw.edu.pl.pap.screenComponents.groupScreens
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import kotlinx.coroutines.launch
+import pw.edu.pl.pap.data.uiSetup.inputFields.InputFieldData
+import pw.edu.pl.pap.screenComponents.BaseScreenComponent
+
+open class InvitationsScreenComponent(
+    baseComponent: BaseScreenComponent,
+    val onDismiss: () -> Unit
+) : BaseScreenComponent by baseComponent {
+
+    var isNewInvitationsScreen: MutableState<Boolean> = mutableStateOf(false)
+
+    private val _newInvitationInputFieldsData = mutableStateListOf<InputFieldData>()
+    val newInvitationInputFieldsData: List<InputFieldData> get() = _newInvitationInputFieldsData
+
+    private val _pendingInvitationInputFieldsData = mutableStateListOf<InputFieldData>()
+    val pendingInvitationInputFieldsData: List<InputFieldData> get() = _pendingInvitationInputFieldsData
+
+    protected open var name: MutableState<String> = mutableStateOf("")
+    protected open var surname: MutableState<String> = mutableStateOf("")
+
+    fun setupNewInvitationInputFields() {
+        _newInvitationInputFieldsData.clear()
+        _newInvitationInputFieldsData.addAll(
+            listOf(
+                InputFieldData.TextFieldData(
+                    title = "Name: ",
+                    parameter = name,
+                    onChange = {
+                        coroutineScope.launch { name.value = it }
+                    }
+                ),
+                InputFieldData.TextFieldData(
+                    title = "Name: ",
+                    parameter = surname,
+                    onChange = {
+                        coroutineScope.launch { surname.value = it }
+                    }
+                ),
+            )
+        )
+    }
+
+}
--- frontend/composeApp/src/commonMain/kotlin/pw/edu/pl/pap/screenComponents/groupScreens/invitations/NewInvitationScreenComponent.kt
+++
@@ -1,9 +0,0 @@
-package pw.edu.pl.pap.screenComponents.groupScreens.invitations
-
-import pw.edu.pl.pap.screenComponents.BaseScreenComponent
-
-open class NewInvitationScreenComponent(
-    baseComponent: BaseScreenComponent,
-    val onDismiss: () -> Unit,
-    val onSave: () -> Unit
-) : BaseScreenComponent by baseComponent
