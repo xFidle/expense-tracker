375a576e28e2fbbe2a415c9a87d09b21a60e2040
Kamil Marszałek <01187250@pw.edu.pl>
2025-01-08 13:39:57 +0100

    Added new save method to TemporaryMembershipService

--- ExpenseAPI/src/main/java/com/example/expenseapi/service/TemporaryMembershipService.java
+++ ExpenseAPI/src/main/java/com/example/expenseapi/service/TemporaryMembershipService.java
@@ -1,9 +1,11 @@
 package com.example.expenseapi.service;
 
+import com.example.expenseapi.dto.MembershipCreateDTO;
 import com.example.expenseapi.pojo.TemporaryMembership;
 
 import java.util.List;
 
 public interface TemporaryMembershipService extends GenericService<TemporaryMembership, Long> {
     List<TemporaryMembership> getByUserId(Long userId);
+    TemporaryMembership save(MembershipCreateDTO temporaryMembershipCreateDTO);
 }
