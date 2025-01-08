2a1a33b3d8f2e5e363fa3c4d3da3fda9892e44cc
Kamil Marszałek <01187250@pw.edu.pl>
2025-01-08 13:40:11 +0100

    Implemented save method to TemporaryMembershipService

--- ExpenseAPI/src/main/java/com/example/expenseapi/service/TemporaryMembershipServiceImpl.java
+++ ExpenseAPI/src/main/java/com/example/expenseapi/service/TemporaryMembershipServiceImpl.java
@@ -1,6 +1,10 @@
 package com.example.expenseapi.service;
 
+import com.example.expenseapi.dto.MembershipCreateDTO;
+import com.example.expenseapi.mapper.UserMapper;
 import com.example.expenseapi.pojo.TemporaryMembership;
+import com.example.expenseapi.repository.GroupRepository;
+import com.example.expenseapi.repository.RoleRepository;
 import com.example.expenseapi.repository.TemporaryMembershipRepository;
 import org.springframework.stereotype.Service;
 
@@ -8,14 +12,30 @@ import java.util.List;
 
 @Service
 public class TemporaryMembershipServiceImpl extends GenericServiceImpl<TemporaryMembership, Long> implements TemporaryMembershipService {
-    TemporaryMembershipRepository temporaryMembershipRepository;
-    public TemporaryMembershipServiceImpl(TemporaryMembershipRepository repository) {
+    private final TemporaryMembershipRepository temporaryMembershipRepository;
+    private final UserMapper userMapper;
+    private final RoleRepository roleRepository;
+    private final GroupRepository groupRepository;
+
+    public TemporaryMembershipServiceImpl(TemporaryMembershipRepository repository, UserMapper userMapper, RoleRepository roleRepository, GroupRepository groupRepository) {
         super(repository);
         this.temporaryMembershipRepository = repository;
+        this.userMapper = userMapper;
+        this.roleRepository = roleRepository;
+        this.groupRepository = groupRepository;
     }
 
     @Override
     public List<TemporaryMembership> getByUserId(Long userId) {
         return temporaryMembershipRepository.findByUserId(userId);
+    }
+
+    @Override
+    public TemporaryMembership save(MembershipCreateDTO temporaryMembershipCreateDTO) {
+        TemporaryMembership temporaryMembership = new TemporaryMembership();
+        temporaryMembership.setUser(userMapper.UserDTOToUser(temporaryMembershipCreateDTO.getUser()));
+        temporaryMembership.setGroup(groupRepository.findById(temporaryMembershipCreateDTO.getGroup().getId()).get());
+        temporaryMembership.setRole(roleRepository.findById(2L).get());
+        return temporaryMembershipRepository.save(temporaryMembership);
     }
 }
