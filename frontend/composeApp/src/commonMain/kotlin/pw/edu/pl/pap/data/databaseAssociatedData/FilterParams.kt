55d40517c3aa7ce3d350cfeaf011240c52ecde49
ddsouza <damian.dsouza.stud@pw.edu.pl>
2025-01-11 01:42:52 +0100

    Added new ExpensePageInfo query data class and implemented toMap extension function fro FilterParams

--- frontend/composeApp/src/commonMain/kotlin/pw/edu/pl/pap/data/databaseAssociatedData/FilterParams.kt
+++ frontend/composeApp/src/commonMain/kotlin/pw/edu/pl/pap/data/databaseAssociatedData/FilterParams.kt
@@ -2,10 +2,9 @@ package pw.edu.pl.pap.data.databaseAssociatedData
 
 import kotlinx.datetime.LocalDate
 import kotlinx.serialization.Serializable
+import kotlin.reflect.KProperty1
 
-sealed class FilterParams() {
-    abstract fun toMap(): Map<String, String>
-}
+abstract class FilterParams() {}
 
 @Serializable
 data class ChartFilterParams(
@@ -14,14 +13,25 @@ data class ChartFilterParams(
     val categoryNames: List<String>? = null,
     val emails: List<String>? = null,
     val methods: List<String>? = null
-) : FilterParams() {
-    override fun toMap(): Map<String, String> {
-        return mapOf(
-            "beginDate" to beginDate?.toString().orEmpty(),
-            "endDate" to endDate?.toString().orEmpty(),
-            "categoryNames" to categoryNames?.toString().orEmpty(),
-            "emails" to emails?.toString().orEmpty(),
-            "methods" to methods?.toString().orEmpty()
-        )
+) : FilterParams()
+
+@Serializable
+data class NextExpensePageInfo(
+    val lastId: Long? = null,
+    val lastKey: String? = null,
+    val size: Int = 20,
+    var order: String
+) : FilterParams()
+
+
+fun FilterParams.toMap(): Map<String, String?> {
+    val result = mutableMapOf<String, String?>()
+
+    this::class.members.filterIsInstance<KProperty1<FilterParams, *>>().forEach { property ->
+        val key = property.name
+        val value = property.get(this)?.toString()
+        result[key] = value
     }
+
+    return result
 }
