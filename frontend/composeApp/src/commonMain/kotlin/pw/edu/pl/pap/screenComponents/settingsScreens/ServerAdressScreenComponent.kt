d880c0d7a5b133aa71ea9207e3a871af83bedc5f
ddsouza <damian.dsouza.stud@pw.edu.pl>
2025-01-11 01:33:47 +0100

    temporarily disabled

--- frontend/composeApp/src/commonMain/kotlin/pw/edu/pl/pap/screenComponents/settingsScreens/ServerAdressScreenComponent.kt
+++ frontend/composeApp/src/commonMain/kotlin/pw/edu/pl/pap/screenComponents/settingsScreens/ServerAdressScreenComponent.kt
@@ -12,35 +12,35 @@ class ServerAdressScreenComponent(
 ) : BaseSettingsScreenComponentImpl(baseSettingsScreenComponent) {
 
 
-    private var serverAddress: MutableState<String> = mutableStateOf(apiService.getCurrentUrl())
-
-    override var confirmationData = ConfirmationDialogConfig(
-        mainText = "Change Server Address",
-        subText = "Are you sure you want to change server address?",
-        onNo = { showConfirmationDialog.value = false },
-        onYes = {
-            showConfirmationDialog.value = false
-            coroutineScope.launch { postChanges() }
-            onBack()
-        }
-    )
-
-    override fun postChanges() {
-        apiService.updateBaseUrl(serverAddress.value)
-    }
-
-    override fun setupInputFields() {
-        _inputFieldsData.clear()
-        _inputFieldsData.addAll(
-            listOf(
-                InputFieldData.TextFieldData(
-                    title = "Server address: ",
-                    parameter = serverAddress,
-                    onChange = {
-                        coroutineScope.launch { serverAddress.value = it }
-                    }
-                )
-            )
-        )
-    }
+//    private var serverAddress: MutableState<String> = mutableStateOf(apiService.getCurrentUrl())
+//
+//    override var confirmationData = ConfirmationDialogConfig(
+//        mainText = "Change Server Address",
+//        subText = "Are you sure you want to change server address?",
+//        onNo = { showConfirmationDialog.value = false },
+//        onYes = {
+//            showConfirmationDialog.value = false
+//            coroutineScope.launch { postChanges() }
+//            onBack()
+//        }
+//    )
+//
+//    override fun postChanges() {
+//        apiService.updateBaseUrl(serverAddress.value)
+//    }
+//
+//    override fun setupInputFields() {
+//        _inputFieldsData.clear()
+//        _inputFieldsData.addAll(
+//            listOf(
+//                InputFieldData.TextFieldData(
+//                    title = "Server address: ",
+//                    parameter = serverAddress,
+//                    onChange = {
+//                        coroutineScope.launch { serverAddress.value = it }
+//                    }
+//                )
+//            )
+//        )
+//    }
 }
