e74f9643c8f04462173631ee3b813e4084a441b0
ddsouza <damian.dsouza.stud@pw.edu.pl>
2025-01-11 01:10:02 +0100

    Deleted BaseScreenComponent.kt

--- frontend/composeApp/src/commonMain/kotlin/pw/edu/pl/pap/screenComponents/BaseScreenComponent.kt
+++
@@ -1,8 +0,0 @@
-package pw.edu.pl.pap.screenComponents
-
-import com.arkivanov.decompose.ComponentContext
-import pw.edu.pl.pap.api.ApiService
-
-interface BaseScreenComponent : ComponentContext, BaseComponent {
-    val apiService: ApiService
-}
