77e80ae08993796790c29e0466193de1dad00b0a
Kamil Marszałek <01187250@pw.edu.pl>
2025-01-11 22:04:53 +0100

    Added lacking service annotation

--- ExpenseAPI/src/main/java/com/example/expenseapi/service/RoleServiceImpl.java
+++ ExpenseAPI/src/main/java/com/example/expenseapi/service/RoleServiceImpl.java
@@ -2,7 +2,9 @@ package com.example.expenseapi.service;
 
 import com.example.expenseapi.pojo.Role;
 import com.example.expenseapi.repository.RoleRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public class RoleServiceImpl extends GenericServiceImpl<Role, Long> implements RoleService {
     public RoleServiceImpl(RoleRepository repository) {
         super(repository);
