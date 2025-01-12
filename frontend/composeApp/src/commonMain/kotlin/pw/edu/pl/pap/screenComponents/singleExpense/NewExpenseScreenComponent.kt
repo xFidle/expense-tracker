d1c966818315e2e3ee29ae64d21468ca43f1ae6e
ddsouza <damian.dsouza.stud@pw.edu.pl>
2025-01-12 22:43:25 +0100

    added prefs to new expense screen

--- frontend/composeApp/src/commonMain/kotlin/pw/edu/pl/pap/screenComponents/singleExpense/NewExpenseScreenComponent.kt
+++ frontend/composeApp/src/commonMain/kotlin/pw/edu/pl/pap/screenComponents/singleExpense/NewExpenseScreenComponent.kt
@@ -1,9 +1,16 @@
 package pw.edu.pl.pap.screenComponents.singleExpense
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import kotlinx.coroutines.launch
+import org.koin.core.component.inject
+import pw.edu.pl.pap.data.databaseAssociatedData.Currency
 import pw.edu.pl.pap.data.databaseAssociatedData.NewExpense
+import pw.edu.pl.pap.data.databaseAssociatedData.PaymentMethod
 import pw.edu.pl.pap.data.databaseAssociatedData.UserGroup
+import pw.edu.pl.pap.repositories.data.UserRepository
 import pw.edu.pl.pap.screenComponents.BaseComponent
+import pw.edu.pl.pap.util.listFunctions.getIndexByField
 
 class NewExpenseScreenComponent(
     baseComponent: BaseComponent,
@@ -11,6 +18,14 @@ class NewExpenseScreenComponent(
     private val currentUserGroup: UserGroup,
 ) : BaseExpenseScreenComponent(baseComponent, onBack) {
 
+    private val userRepository: UserRepository by inject()
+    private val preferences = userRepository.currentPreferences.value
+
+    override var currencyIndex: MutableState<Int> =
+        mutableStateOf(getIndexByField(currencies, preferences?.currencySymbol, Currency::symbol) ?: 0)
+
+    override var methodOfPaymentIndex: MutableState<Int> =
+        mutableStateOf(getIndexByField(methodsOfPayment, preferences?.methodOfPayment, PaymentMethod::name) ?: 0)
 
     override fun confirm() {
         val newExpense = NewExpense(
