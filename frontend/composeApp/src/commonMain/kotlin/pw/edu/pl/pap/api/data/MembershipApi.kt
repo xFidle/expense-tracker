99b1cbbad42ecc212ddd4f7d7b41c8e1aaf56424
msuski <01187288@pw.edu.pl>
2025-01-12 14:26:46 +0100

    Added kickMember to MembershipApi

--- frontend/composeApp/src/commonMain/kotlin/pw/edu/pl/pap/api/data/MembershipApi.kt
+++ frontend/composeApp/src/commonMain/kotlin/pw/edu/pl/pap/api/data/MembershipApi.kt
@@ -1,7 +1,9 @@
 package pw.edu.pl.pap.api.data
 
 import de.jensklingenberg.ktorfit.http.Body
+import de.jensklingenberg.ktorfit.http.DELETE
 import de.jensklingenberg.ktorfit.http.POST
+import de.jensklingenberg.ktorfit.http.Path
 import io.ktor.client.statement.*
 import pw.edu.pl.pap.data.databaseAssociatedData.InviteRequest
 
@@ -10,4 +12,6 @@ interface MembershipApi {
     @POST("membership/invite")
     suspend fun sendInvite(@Body request: InviteRequest): HttpResponse
 
+    @DELETE("membership/delete/{userId}/{groupName}")
+    suspend fun kickMember(@Path("userId") userId: Long, @Path("groupName") groupName: String): HttpResponse
 }
--- frontend/composeApp/src/commonMain/kotlin/pw/edu/pl/pap/repositories/data/MembershipRepository.kt
+++ frontend/composeApp/src/commonMain/kotlin/pw/edu/pl/pap/repositories/data/MembershipRepository.kt
@@ -9,8 +9,15 @@ class MembershipRepository(val api: MembershipApi) {
 
     suspend fun invite(user: User, group: UserGroup){
         try {
-            println(api.sendInvite(InviteRequest(user, group)))
-            //TODO remove print when debugged
+            api.sendInvite(InviteRequest(user, group))
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    suspend fun kickMember(user: User, group: UserGroup) {
+        try {
+            api.kickMember(user.id, group.name)
         } catch (e: Exception) {
             e.printStackTrace()
         }
